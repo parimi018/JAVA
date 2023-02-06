730a4a0a8aaac84dfbfce172500f8f47855c205a
parimi018 <108624499+parimi018@users.noreply.github.com>
2023-02-06 14:36:56 +0530

    Update Label.java

--- Week-15/Label.java
+++ Week-15/Label.java
@@ -1,1 +1,25 @@
+import java.awt.*;
+import java.applet.*;
+
+/* <applet code="Label" width=300 height=200></applet> */
+
+public class Label extends Applet 
+{
+    Label one,two,three;
+
+    public void init() 
+    {
+        setBackground(Color.black);
+        setForeground(Color.white);
+        
+        one = new Label("One",Label.RIGHT);
+        two = new Label("Two",Label.CENTER);
+        three = new Label("Three",Label.RIGHT);
+
+        // add labels to applet window
+        add(one);
+        add(two);
+        add(three);
+    }
+}
 
