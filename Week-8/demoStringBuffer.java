6f05f235c844285e111bd88bbec66e760c910062
Shreyas Parimi <108624499+parimi018@users.noreply.github.com>
2023-02-08 09:44:15 +0530

    Add files via upload

---
+++ Week-8/demoStringBuffer.java
@@ -0,0 +1,15 @@
+class test
+{
+	public static void main(String args[])
+	{
+		int a=5;
+		a+=6;
+		switch(a-1)
+		{
+			case 5: System.out.print("10"); break;
+			case 10:System.out.print("15");
+				System.out.print(((a%2==0)?"-even-":"-odd-"));
+			default:System.out.print(a%2);
+		}
+	}
+}
