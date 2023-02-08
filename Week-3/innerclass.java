53ac1067aaf5b347d3b2ac2d380df81396d48ba4
Shreyas Parimi <108624499+parimi018@users.noreply.github.com>
2023-02-08 09:30:16 +0530

    Update innerclass.java

--- Week-3/innerclass.java
+++ Week-3/innerclass.java
@@ -1,1 +1,25 @@
-xx
+class Outer
+{
+int outer_x=2;
+void test()
+{
+Inner i=new Inner();
+outer_x+=2;
+i.display();
+}
+class Inner
+{
+void display()
+{
+System.out.println("Outer_x="+outer_x);
+}
+}
+}
+class InnerclassDemo
+{
+public static void main(String args[])
+{
+Outer o=new Outer();
+o.test();
+}
+}
